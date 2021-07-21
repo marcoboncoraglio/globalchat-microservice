d6d679e64c2e74dacb29a3eb62d7127a59d597c7
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2021-07-21 20:20:24 +0200

    adapt chat skeleton

--- server/app.js
+++ server/app.js
@@ -1,13 +1,48 @@
-// Copyright 2018-2020Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
 const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
+exports.onConnect = async event => {
+  const putParams = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      connectionId: event.requestContext.connectionId // chat id
+    }
+  };
+
+  try {
+    await ddb.put(putParams).promise();
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
+  }
+
+  return { statusCode: 200, body: 'Connected.' };
+};
+
+exports.onDisconnect = async event => {
+  const deleteParams = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      connectionId: event.requestContext.connectionId
+    }
+  };
+
+  try {
+    await ddb.delete(deleteParams).promise();
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to disconnect: ' + JSON.stringify(err) };
+  }
+
+  return { statusCode: 200, body: 'Disconnected.' };
+};
+
+
 const { TABLE_NAME } = process.env;
 
-exports.handler = async event => {
+exports.sendMessage = async event => {
   let connectionData;
   
   try {
--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,3 +1,3 @@
-cd onconnect && ncu -u && npm install && cd ..
-cd ondisconnect && ncu -u && npm install && cd ..
-cd sendmessage && ncu -u && npm install && cd ..
+cd onconnect && npm install && cd ..
+cd ondisconnect && npm install && cd ..
+cd sendmessage && npm install && cd ..
