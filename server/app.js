7ff206eb60d6c6f0f4c6d9ab7215d28e35625e59
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2021-08-03 14:13:44 +0200

    add changeroom handler

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const onConnect = require('./onconnect/onconnect');
 const onDisconnect = require('./ondisconnect/ondisconnect');
 const onSendMessage = require('./sendmessage/sendmessage');
+const changeRoom = require('./changeRoom/changeRoom');
 // const messagesApi = require('./messagesapi/messagesapi');
+
 
 exports.onConnect = onConnect;
 
 exports.onDisconnect = onDisconnect;
 
 exports.sendMessage = onSendMessage;
+
+exports.changeRoom = changeRoom;
 
 // exports.messagesApi = messagesApi;
