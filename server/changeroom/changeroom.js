230cf25cc13aa82078206cbf065cc049db160f07
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2021-08-09 17:02:44 +0200

    return connectionId, number of connected user to onRoomChange

--- server/changeroom/changeroom.js
+++ server/changeroom/changeroom.js
@@ -5,14 +5,17 @@ const ddb = new AWS.DynamoDB.DocumentClient({
   region: process.env.AWS_REGION,
 });
 
+const ApiGatewayManagementApi = require('aws-sdk').ApiGatewayManagementApi;
+
 module.exports = async (event) => {
-  const body = JSON.parse(event.body)
+  const body = JSON.parse(event.body);
+  const connectionId = event.requestContext.connectionId;
   const { chatUrl } = body;
 
   const putParams = {
     TableName: process.env.CONNECTIONS_TABLE_NAME,
     Item: {
-      connectionId: event.requestContext.connectionId, // socket id
+      connectionId,
       chatUrl,
     },
   };
@@ -45,10 +48,38 @@ module.exports = async (event) => {
     return { statusCode: 500, body: e.stack };
   }
 
+  const apigwManagementApi = new ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint:
+      event.requestContext.domainName + '/' + event.requestContext.stage,
+  });
+
   const returnMsg = {
-    body: 'Connected to ' + chatUrl,
+    message: 'Connected to ' + chatUrl,
     roomCount: connectionIds.Items.length,
+    connectionId: connectionId,
   };
 
-  return { statusCode: 200, body: JSON.stringify(returnMsg) };
+  try {
+    await apigwManagementApi
+      .postToConnection({
+        ConnectionId: connectionId,
+        Data: JSON.stringify(returnMsg),
+      })
+      .promise();
+  } catch (e) {
+    if (e.statusCode === 410) {
+      console.log(`Found stale connection, deleting ${connectionId}`);
+      await ddb
+        .delete({
+          TableName: process.env.CONNECTIONS_TABLE_NAME,
+          Key: { connectionId },
+        })
+        .promise();
+    } else {
+      throw e;
+    }
+  }
+
+  return { statusCode: 200, body: 'Changed room' };
 };
